e3756637bf005dd54759c6aecdb89c2b0ae9ce13
prisca1130 <niainaramanantoanina@yahoo.fr>
2021-12-11 19:50:00 +0100

    Recuperation check

--- Pirate/projetPirate/Pirate2/src/up/mi/jr/Recuperation.java
+++ Pirate/projetPirate/Pirate2/src/up/mi/jr/Recuperation.java
@@ -10,240 +10,137 @@ import java.util.ArrayList;
 
 public class Recuperation {
 	private File fichier;
+	private Relation rel;
 	
-	public Recuperation(File fichier) {
+	public Recuperation(File fichier, Relation rel) {
 		this.fichier = fichier;
+		this.rel=rel;
 	}
-	
-	public void parser(Relation rel) {
-		String nom = null;
+
+	public void parserObjet() {
 		String nomObjet = null;
-		String nomsPirate = null;
-		String nomsPirObj = null;
-		
+
 		try(BufferedReader br = new BufferedReader(new FileReader(fichier))){
 			String ligne = null;
 			while ((ligne = br.readLine()) != null) {
-				
-				if(ligne.startsWith("pirate") || ligne.startsWith("Pirate")) {
-					nom = ligne.substring(ligne.indexOf("(")+1, ligne.indexOf(")"));
-					rel.getListePirate().put(nom,new Pirate(nom));
-					System.out.println(rel.afficheListepirate());
-				}
-				
-				
+
 				if(ligne.startsWith("objet") || ligne.startsWith("Objet")) {
+
 					nomObjet = ligne.substring(ligne.indexOf("(")+1, ligne.indexOf(")"));
 					rel.getListeObj().put(nomObjet,new Objet(nomObjet));
-					System.out.println(rel.afficheListeObj());
 				}
-				
+			}
+		}catch (FileNotFoundException e) {
+			e. printStackTrace ();
+		} catch (IOException e) {
+			e. printStackTrace ();
+		}
+		if(nomObjet == null) {
+			System.err.println("Le nom de l'objet n'a pas ete indiqué");
+			System.exit(0);
+		}
+		if (rel.getListeObj().size() != rel.getListePirate().size()) {
+			System.err.println("Attention le nombre d'objet saisi est different du nombre de pirate saisi");
+			System.err.println("Veuillez revoir votre fichier");
+		}
+
+	}
+
+
+	public void parserDeteste() throws NullPointerException{
+		String nomsPirate = null;
+
+		try(BufferedReader br = new BufferedReader(new FileReader(fichier))){
+			String ligne = null;
+			while ((ligne = br.readLine()) != null) {
+
 				if(ligne.startsWith("deteste") || ligne.startsWith("Deteste")) {
 					nomsPirate = (ligne.substring(ligne.indexOf("(")+1, ligne.indexOf(")")));
-//					//Traiter le cas où il rentre pls desteste ex 3
-					ArrayList<Pirate> deteste1 = new ArrayList<Pirate>();
-					ArrayList<Pirate> deteste2 = new ArrayList<Pirate>();
-					deteste1.add(rel.getListePirate().get(nomsPirate.split(",")[1]));
-					deteste2.add(rel.getListePirate().get(nomsPirate.split(",")[0]));
-					rel.getdeteste().put(rel.getListePirate().get(nomsPirate.split(",")[0]),deteste1);
-					rel.getdeteste().put(rel.getListePirate().get(nomsPirate.split(",")[1]),deteste2);
-					System.out.println(rel.afficheDeteste());
-					
+//						//Traiter le cas où il rentre pls desteste ex 3
+					ArrayList<Pirate> deteste1 = new ArrayList<>();
+					ArrayList<Pirate> deteste2 = new ArrayList<>();
+					if(!(rel.getListePirate().containsKey(nomsPirate.split(",")[0]) && rel.getListePirate().containsKey(nomsPirate.split(",")[1]))){
+						throw new NullPointerException("Veuillez revoir le fichier au niveau de \"deteste\", car le nom du pirate n'existe pas");
+					}
+					if(nomsPirate.split(",")[0].equals(nomsPirate.split(",")[1])) {
+						System.err.println("Veuillez revoir le fichier au niveau de \"deteste\" \nUn pirate ne peut se détester lui-même");
+					}
+					Pirate p1 = rel.getListePirate().get(nomsPirate.split(",")[0]);
+					Pirate p2 = rel.getListePirate().get(nomsPirate.split(",")[1]);
+					if(rel.getdeteste().containsKey(p1)){
+						rel.getdeteste().get(p1).add(p2);
+					}else {
+						deteste1.add(p2);
+						rel.getdeteste().put(p1, deteste1);
+					}
+					if(rel.getdeteste().containsKey(p2)){
+						rel.getdeteste().get(p2).add(p1);
+					}else {
+						deteste2.add(p1);
+						rel.getdeteste().put(p2, deteste2);
+					}
+
 				}
-				
-				if(ligne.startsWith("preferences") || ligne.startsWith("Preferences") || ligne.startsWith("preference") || ligne.startsWith("preference")) {
+			}
+		}catch (FileNotFoundException e) {
+			e. printStackTrace ();
+		} catch (IOException e) {
+			e. printStackTrace ();
+		}
+		if(nomsPirate == null) {
+			System.err.println("La relation d'affinité entre les pirates n'a pas ete indiqué");
+			System.exit(0);
+		}
+
+	}
+
+
+	public void parserPreference() {
+		String nomsPirObj = null;
+
+		try(BufferedReader br = new BufferedReader(new FileReader(fichier))){
+			String ligne = null;
+			while ((ligne = br.readLine()) != null) {
+
+				if(ligne.startsWith("preferences") || ligne.startsWith("Preferences")) {
+
 					nomsPirObj = (ligne.substring(ligne.indexOf("(")+1, ligne.indexOf(")")));
 
 					//A Traiter le cas où l'utilisateur oublie d'entrer le nom des 3 objets dans preference
-					ArrayList<Objet> listObj = new ArrayList<Objet>();
+					ArrayList<Objet> listObj = new ArrayList<>();
 					String [] EnsPirObj = nomsPirObj.split(",");
-					for(int i = 1; i<EnsPirObj.length; i++) {
-						listObj.add(new Objet(nomsPirObj.split(",")[i]));
-						//System.out.println(EnsPirObj[0]);
+					if (EnsPirObj.length != rel.getListePirate().size()+1 ) {
+						System.err.println("Attention, tous les objets doivent être ordonner dans la liste de préférence de chaque pirate");
+						System.err.println("Veuillez revoir votre fichier, au niveau de preference");
 					}
-					rel.getPreference().put(rel.getListePirate().get(EnsPirObj[0]), listObj);
-					System.out.println(rel.affichePreference());
+
+					for(int i = 1; i<EnsPirObj.length; i++) {
+						if(listObj.contains(rel.getListeObj().get(nomsPirObj.split(",")[i]))){
+							System.err.println("Veuillez revoir votre fichier, au niveau de preference, car vous avez saisi deux fois le même objet");
+						}else {
+							listObj.add(rel.getListeObj().get(nomsPirObj.split(",")[i]));
+						}
+						rel.getPreference().put(rel.getListePirate().get(EnsPirObj[0]), listObj);
+						//System.out.println(rel.affichePreference());
+					}
+
 				}
-			
-				
 			}
-		}catch (FileNotFoundException e) { 
+		}catch (FileNotFoundException e) {
 			e. printStackTrace ();
-		} catch (IOException e) { 
+		} catch (IOException e) {
 			e. printStackTrace ();
 		}
-		
-		
-//		if (nom == null) {
-//			System.err.println("Le nom du pirate n°" + listePirate.size()+1 + " n’a pas été indique"); 
-//			System.exit(0);
-//		}
-//		//return listePirate;
-//		
-//		if (nom == null) {
-//			System.err.println("Le nom de l'objet n°" + listeObjet.size()+1 + " n’a pas été indique"); 
-//			System.exit(0);
-//		}
-//		
-//		//A corriger
-//		if (nomsPirate == null) {
-//			System.err.println("La saisie de deteste est mal indiquée"); 
-//			System.exit(0);
-//		}
-//	
-//		//A corriger
-//		if (nomsPirObj == null) {
-//			System.err.println("La saisie de preference est mal indiquée"); 
-//			System.exit(0);
-//		}
-//		
-//		Relation rel = new Relation();
-//		rel.setDeteste(seDeteste);
-//		rel.setPreference(preferenceObj);
-//		return rel;
+		if(nomsPirObj == null) {
+			System.err.println("La liste de preferences d'un pirate n'a pas ete indiqué");
+			System.exit(0);
+		}
+		if (rel.getPreference().size() != rel.getListePirate().size()) {
+			System.err.println("Attention, il faut saisir autant de relation de préférence que de nombre de pirate");
+			System.err.println("Veuillez revoir votre fichier, au niveau de preference");
+		}
+
 	}
-//	
-	
-	
-//	public ArrayList<Pirate> parserPirate(File fichier) {
-//		ArrayList<Pirate> listePirate = new ArrayList<Pirate>();
-//		String nom = null;
-//		
-//		try(BufferedReader br = new BufferedReader(new FileReader(fichier))){
-//			String ligne = null;
-//			while ((ligne = br.readLine()) != null) {
-//				if(ligne.startsWith("pirate") || ligne.startsWith("Pirate")) {
-////					if(nom != null) {
-////						System.err.println("Il ne peut pas y avoir deux lignes pour le nom d'un pirate");
-////						System.exit(0);
-////					}else {
-////						nom = ligne.substring(ligne.indexOf("(")+1, ligne.indexOf(")"));
-////						listePirate.add(new Pirate(nom));
-////					}
-//					
-//					nom = ligne.substring(ligne.indexOf("(")+1, ligne.indexOf(")"));
-//					listePirate.add(new Pirate(nom));
-//				}
-//			}
-//		}catch (FileNotFoundException e) { 
-//			e. printStackTrace ();
-//		} catch (IOException e) { 
-//			e. printStackTrace ();
-//		}
-//		if (nom == null) {
-//			System.err.println("Le nom du pirate n°" + listePirate.size()+1 + " n’a pas été indique"); 
-//			System.exit(0);
-//		}
-//		return listePirate;
-//	}
-//	
-//	public ArrayList<Objet> parserObjet(File fichier) {
-//		ArrayList<Objet> listeObjet = new ArrayList<Objet>();
-//		String nom = null;
-//		
-//		try(BufferedReader br = new BufferedReader(new FileReader(fichier))){
-//			String ligne = null;
-//			while ((ligne = br.readLine()) != null) {
-//				if(ligne.startsWith("objet") || ligne.startsWith("Objet")) {
-////					if(nom != null) {
-////						System.err.println("Il ne peut pas y avoir deux lignes pour le nom d'un pirate");
-////						System.exit(0);
-////					}else {
-////						nom = ligne.substring(ligne.indexOf("(")+1, ligne.indexOf(")"));
-////						listeObjet.add(new Objet(nom));
-////					}
-//					
-//					nom = ligne.substring(ligne.indexOf("(")+1, ligne.indexOf(")"));
-//					listeObjet.add(new Objet(nom));
-//				}
-//			}
-//		}catch (FileNotFoundException e) { 
-//			e. printStackTrace ();
-//		} catch (IOException e) { 
-//			e. printStackTrace ();
-//		}
-//		if (nom == null) {
-//			System.err.println("Le nom de l'objet n°" + listeObjet.size()+1 + " n’a pas été indique"); 
-//			System.exit(0);
-//		}
-//		return listeObjet;
-//	}
-//	
-//	public HashMap<Pirate, ArrayList<Pirate>> parserDeteste(File fichier) {
-//		HashMap<Pirate, ArrayList<Pirate>> seDeteste= new HashMap<Pirate, ArrayList<Pirate>>();
-//		//ArrayList<Pirate> seDeteste = new ArrayList<Pirate>();
-//		String nomsPirate = null;
-//		
-//		try(BufferedReader br = new BufferedReader(new FileReader(fichier))){
-//			String ligne = null;
-//			while ((ligne = br.readLine()) != null) {
-//				if(ligne.startsWith("deteste") || ligne.startsWith("Deteste")) {
-//					nomsPirate = (ligne.substring(ligne.indexOf("(")+1, ligne.indexOf(")")));
-//
-//
-//					//A Traiter le cas où l'utilisateur oublie d'entrer le nom du 2eme pirate dans deteste 
-//					ArrayList<Pirate> deteste1 = new ArrayList<Pirate>();
-//					ArrayList<Pirate> deteste2 = new ArrayList<Pirate>();
-//					deteste1.add(new Pirate(nomsPirate.split(",")[1]));
-//					deteste2.add(new Pirate(nomsPirate.split(",")[0]));
-//					seDeteste.put(new Pirate(nomsPirate.split(",")[0]), deteste1);
-//					seDeteste.put(new Pirate(nomsPirate.split(",")[1]), deteste2);
-//					
-//				}
-//			}
-//		}catch (FileNotFoundException e) { 
-//			e. printStackTrace ();
-//		} catch (IOException e) { 
-//			e. printStackTrace ();
-//		}
-//		//A corriger
-//		if (nomsPirate == null) {
-//			System.err.println("La saisie de deteste est mal indiquée"); 
-//			System.exit(0);
-//		}
-//		return seDeteste;
-//	}
-//	
-//	public HashMap<Pirate, ArrayList<Objet>> parserPreference(File fichier) {
-//		HashMap<Pirate, ArrayList<Objet>> preferenceObj= new HashMap<Pirate, ArrayList<Objet>>();
-//		String nomsPirObj = null;
-//		
-//		try(BufferedReader br = new BufferedReader(new FileReader(fichier))){
-//			String ligne = null;
-//			while ((ligne = br.readLine()) != null) {
-//				if(ligne.startsWith("deteste") || ligne.startsWith("Deteste")) {
-//					nomsPirObj = (ligne.substring(ligne.indexOf("(")+1, ligne.indexOf(")")));
-//
-//					//A Traiter le cas où l'utilisateur oublie d'entrer le nom des 3 objets dans preference
-//					ArrayList<Objet> listObj = new ArrayList<Objet>();
-//					String [] EnsPirObj = nomsPirObj.split(",");
-//					for(int i = 1; i<EnsPirObj.length; i++) {
-//						listObj.add(new Objet(nomsPirObj.split(",")[i]));
-//					}
-//					preferenceObj.put(new Pirate(EnsPirObj[0]), listObj);
-//					
-//				}
-//			}
-//		}catch (FileNotFoundException e) { 
-//			e. printStackTrace ();
-//		} catch (IOException e) { 
-//			e. printStackTrace ();
-//		}
-//		//A corriger
-//		if (nomsPirObj == null) {
-//			System.err.println("La saisie de preference est mal indiquée"); 
-//			System.exit(0);
-//		}
-//		return preferenceObj;
-//	}
-	
-	//A embellir
-//	public Relation recupTotal(Relation rel) {
-//		//Relation rel = new Relation();
-//		rel.setDeteste(parser(fichier));
-//		rel.setPreference(parserPreference(fichier));
-//		return rel;
-//	}
 	
 	
 }
